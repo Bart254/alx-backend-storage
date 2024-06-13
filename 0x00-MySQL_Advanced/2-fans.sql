bef9c01f0e9c57b70d51b929d7726e1628280a71
Bart254 <omondibartlett@gmail.com>
2024-06-13 19:51:10 +0300

    correct 2-fans.sql
    Initially I created an index but the execution of the query doesn't necessitate one at the moment.

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,7 +1,5 @@
 -- Script ranks country bands based on fans
 -- We group by origin to use SUM function
-CREATE INDEX origin_fan_idx ON metal_bands(origin, fans);
-
 SELECT origin, SUM(fans) as nb_fans
     FROM metal_bands
     GROUP BY origin
