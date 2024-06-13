c4823ee687e8fa478774dc051957af49c6cbe684
Bart254 <omondibartlett@gmail.com>
2024-06-13 13:23:12 +0300

    update file 1-country_users.sql

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,7 +1,5 @@
 -- Create table users
 -- users have attributes: name, email, id and enum of country codes
--- First, drop table if it exists
-DROP TABLE IF EXISTS `users`;
 CREATE TABLE IF NOT EXISTS `users`(
     `id` INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     `email` VARCHAR(255) NOT NULL UNIQUE,
