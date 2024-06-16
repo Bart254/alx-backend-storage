87480d6cfcc68869b1252cd577f4c347f79f5461
Bart254 <omondibartlett@gmail.com>
2024-06-16 16:21:28 +0300

    add safe division function file 10-div.sql

---
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -0,0 +1,14 @@
+-- Creates a function
+-- Function SafeDiv prevents zerodivision error
+DELIMITER $$
+CREATE FUNCTION SafeDiv(a INT, b INT)
+RETURNS INT
+DETERMINISTIC
+BEGIN
+    DECLARE result INT;
+    IF b <= 0 THEN SET result = 0;
+    ELSE SET result = a / b;
+    END IF;
+    RETURN result;
+END$$
+DELIMITER ;
