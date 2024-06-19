12a8b16470edf761e88e6d58748fa379d994c2aa
Bart254 <omondibartlett@gmail.com>
2024-06-19 22:21:29 +0300

    add script exercise.py

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+""" Creates cache class
+"""
+import redis
+from typing import Union
+
+
+class Cache:
+    """ Cache class for redis database
+
+    Attributes:
+        __redis: private instance attribute that stores redis client
+
+    Methods:
+        store: takes a data argument and returns a key
+    """
+    def __init__(self):
+        """ stores an instance of Redis client in a private attribute
+        """
+        self.__redis = redis.Redis()
+
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """ generates a key and stores data passed as argument
+
+        Args:
+            data(str, byte, int, float): data to be stores in Redis
+
+        Returns:
+            key(str): key to data stored
+        """
+        import uuid
+        key: str = str(uuid.uuid4())
+        self.__redis.set(key, data)
+        return key
